6fc74c8662f1b3ecb5b160b4e93b64bfd712a7b9
Evan G <evan@mail.fbievan.live>
2025-11-04 00:21:51 +0000

    Setup basic functions like getword and check

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const fs = require('fs'); // Import the File System module
 
 // Get a random word from the JSON file
 function getword () {
-    const filePath = './words.json'; // Replace with the actual path to your file
-    const fileData = fs.readFileSync(filePath, 'utf8'); // Read the file content synchronously
-    const jsonArray = JSON.parse(fileData).words; // Parse the JSON string into a JavaScript array
-    const randomIndex = Math.floor(Math.random() * jsonArray.length);
+    const filePath = './words.json'; 
+    const fileData = fs.readFileSync(filePath, 'utf8'); // Read the file 
+    const jsonArray = JSON.parse(fileData).words; // Make json into array
+    const randomIndex = Math.floor(Math.random() * jsonArray.length); // Get a random index
     console.log(jsonArray.length);
     const randomWord = jsonArray[randomIndex]; // Get the random word
     return(randomWord);
 }
+// Function to check if character is in word
+function checkAgainstWord(char, word) {
+    return(mainword.toLowerCase().includes(char));
+}
+
 
 const mainword = getword();
+
+console.log(checkAgainstWord('t', mainword));
 
 console.log(mainword);
