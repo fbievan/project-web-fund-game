2d1d71ad03fafee8b9d44e5fd2c3ee3a70542acd
Evan G <evan@mail.fbievan.live>
2025-11-24 05:44:47 +0000

    use data embedded in code for code

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 
 // Get a random word from the JSON file
 function getword () {
-    // Return the promise so callers can receive the random word
-    return fetch('/words2.json', { method: 'GET' })
-        .then(function(response) { return response.json(); })
-        .then(function(json) {
-            const jsonArray = json.words; // Make json into array
-            const randomIndex = Math.floor(Math.random() * jsonArray.length); // Get a random index
-            const randomWord = jsonArray[randomIndex]; // Get the random word
-            // Fix error that was happening sometimes (not 5 letters)
-            if (randomWord.length != 5) {
-                console.log('Retrying for 5 Letter')
-                return getword(); // Retry if not 5 letters (will fetch json again but works fine)
-            }
-            return randomWord;
-        });
-}
+    const json = '{"words":["arbys","count","tart","sweet","pulse"]}'
+    const jsonArray = json.words; // Make json into array
+    const randomIndex = Math.floor(Math.random() * jsonArray.length); // Get a random index
+    const randomWord = jsonArray[randomIndex]; // Get the random word
+    // // Fix error that was happening sometimes (not 5 letters)
+    if (randomWord.length != 5) {
+        console.log('Retrying for 5 Letter')
+        return getword(); // Retry if not 5 letters (will fetch json again but works fine)
+           }
+        return randomWord;
+        };
 // Function to check if character is in word
 function checkAgainstWord(char, word) {
     if (!word) return false;
